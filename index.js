f73828953fed59d7cddda5ae40b2f42f9a5e7f74
Michelle <choi.michelle.12@gmail.com>
2023-02-01 11:38:44 -0500

    passes tests

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
 // Code your solution here
+function findMatching(data, name){
+    return data.filter(e => e.toLowerCase() === name.toLowerCase())
+}
+
+function fuzzyMatch(data, letters){
+    return data.filter(e => {
+        for (const char in letters){
+            if (e[char] !== letters[char]){
+                return false;
+            }
+        }
+        return true;
+    })
+}
+
+const drivers = [
+    {
+      name: 'Bobby',
+      hometown: 'Pittsburgh' },
+    {
+      name: 'Sammy',
+      hometown: 'New York' } ,
+    {
+      name: 'Sally',
+      hometown: 'Cleveland' },
+    {
+      name: 'Annette',
+      hometown: 'Los Angeles' },
+    {
+      name: 'Bobby',
+      hometown: 'Tampa Bay' }
+  ];
+
+function matchName(data, name){
+    return data.filter(e => {
+        if (e.name === name) {
+            return true;
+        }
+        else return false;
+    })};
